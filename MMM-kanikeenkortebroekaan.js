5696445a5d7b94fad34c6ec8422ff6fdf5fe27cc
Bjorn van Beek <bjorn.vanbeek@drukwerkdeal.nl>
2020-09-17 22:10:47 +0200

    Redid structure

--- MMM-kanikeenkortebroekaan.js
+++ MMM-kanikeenkortebroekaan.js
@@ -1,41 +1,40 @@
 Module.register("MMM-kanikeenkortebroekaan", {
-	// Default module config.
-	defaults: {
-		refreshInterval: "1800000" // 30 minutes
-	},
+  // Default module config.
+  defaults: {
+    refreshInterval: "5000" // 30 minutes
+  },
 
-	start: function (){
-		this.imageSrc = 0
-	},
+  start: function () {
+    this.imageSrc = "";
+    this.sendSocketNotification("UPDATE_PLEASE");
+  },
 
-	// Override dom generator.
-	getDom: function () {
-		var image = document.createElement("IMG");
-		image.setAttribute("src", this.imageSrc);
-		image.setAttribute("style", "filter: grayscale(100%); contrast(200%)");
-		image.setAttribute("width", "200em");
-		image.id = "SHORTS_IMAGE";
-		return image;
-	},
+  // Override dom generator.
+  getDom: function () {
+    var image = document.createElement("IMG");
+    image.setAttribute("src", this.imageSrc);
+    image.setAttribute("style", "filter: grayscale(100%); contrast(200%)");
+    image.setAttribute("width", "200em");
+    image.id = "SHORTS_IMAGE";
+    return image;
+  },
 
-	notificationReceived: function (notification) {
-		switch (notification) {
-			case "DOM_OBJECTS_CREATED":
-				console.log("DOM_CREATED");
-				setInterval(() => {
-					this.sendSocketNotification("UPDATE_PLEASE");
-				}, this.config.refreshInterval);
-				break;
-		}
-	},
+  notificationReceived: function (notification) {
+    switch (notification) {
+      case "DOM_OBJECTS_CREATED":
+        setInterval(() => {
+          this.sendSocketNotification("UPDATE_PLEASE");
+        }, this.config.refreshInterval);
+        break;
+    }
+  },
 
-	socketNotificationReceived: function (notification, { imageSrc }) {
-		switch (notification) {
-			case "ANSWER":
-				// var image = document.getElementById("SHORTS_IMAGE");
-				// image.setAttribute("src", imageSrc);
-				this.imageSrc = imageSrc;
-				break;
-		}
-	}
+  socketNotificationReceived: function (notification, { imageSrc }) {
+    switch (notification) {
+      case "ANSWER":
+        this.imageSrc = imageSrc;
+        this.updateDom();
+        break;
+    }
+  }
 });
--- node_helper.js
+++ node_helper.js
@@ -4,23 +4,27 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
 module.exports = NodeHelper.create({
-	start: function () {
-		this.answer = { text: "Geen idee", imageSrc: null };
-	},
-	socketNotificationReceived: function (notification) {
-		switch (notification) {
-			case "UPDATE_PLEASE":
-				axios
-					.get("https://www.kanikeenkortebroekaan.nl/", { timeout: 5000 })
-					.then(({ data }) => {
-						const dom = new JSDOM(data);
-						this.answer.text = dom.window.document.body.className;
-						this.answer.imageSrc = `https://www.kanikeenkortebroekaan.nl/assets/${this.answer.text === "ja" ? "yes" : "no"}-man.png`;
-						console.log("Kan ik een korte broek aan vandaag? ", this.answer);
-						this.sendSocketNotification("ANSWER", this.answer);
-					})
-					.catch((error) => console.log("Error while fetching kanikeenkortebroekaan.nl", error));
-				break;
-		}
-	}
+  start: function () {
+    this.answer = { text: "null", imageSrc: null };
+  },
+  socketNotificationReceived: function (notification) {
+    switch (notification) {
+      case "UPDATE_PLEASE":
+        axios
+          .get("https://www.kanikeenkortebroekaan.nl/", { timeout: 5000 })
+          .then(({ data }) => {
+            const dom = new JSDOM(data);
+            this.answer.text = dom.window.document.body.className;
+            this.answer.imageSrc = `https://www.kanikeenkortebroekaan.nl/assets/${
+              this.answer.text === "ja" ? "yes" : "no"
+            }-man.png`;
+            console.log("Kan ik een korte broek aan vandaag? ", this.answer);
+            this.sendSocketNotification("ANSWER", this.answer);
+          })
+          .catch((error) =>
+            console.log("Error while fetching kanikeenkortebroekaan.nl", error)
+          );
+        break;
+    }
+  }
 });
